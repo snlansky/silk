9264e7885435559ebe9c2725d7bb35ce6af53d63
snlansky <snlan@live.cn>
2020-04-28 19:32:32 +0800

    update ns rwset convert

--- libs/error/src/lib.rs
+++ libs/error/src/lib.rs
@@ -6,7 +6,9 @@ pub type Sender<T> = mpsc::Sender<std::result::Result<T, Status>>;
 
 pub type Receiver<T> = mpsc::Receiver<std::result::Result<T, Status>>;
 
-pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
+pub type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 pub type RpcResult<T> = std::result::Result<Response<T>, Status>;
 
@@ -14,10 +16,10 @@ pub fn into_rpc_response<T>(t: T) -> RpcResult<T> {
     Ok(tonic::Response::new(t))
 }
 
-pub fn into_status(err: Box<dyn std::error::Error + Send + Sync + 'static>) -> Status {
+pub fn into_status(err: Error) -> Status {
     tonic::Status::unknown(err.to_string())
 }
 
-pub fn from_str(info: &str) -> Box<dyn std::error::Error + Send + Sync + 'static> {
-    Box::<dyn std::error::Error + Send + Sync + 'static>::from(info.to_string())
+pub fn from_str(info: &str) -> Error {
+    Error::from(info.to_string())
 }
--- libs/rwset/src/rwset.rs
+++ libs/rwset/src/rwset.rs
@@ -22,17 +22,12 @@ pub struct  CollHashedRwSet {
 }
 
 impl TryFrom<TxReadWriteSet> for TxRwSet {
-    type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+    type Error = Error;
 
     fn try_from(value: TxReadWriteSet) -> Result<Self> {
-        unimplemented!()
-    }
-}
-impl TxRwSet {
-    fn new(proto_msg:TxReadWriteSet) -> Result<Self> {
-        let mut ns_rw_sets = Vec::with_capacity(proto_msg.ns_rwset.len());
-        for msg in proto_msg.ns_rwset {
-            let ns_rw_set = nsRwSetFromProtoMsg(msg)?;
+        let mut ns_rw_sets = Vec::with_capacity(value.ns_rwset.len());
+        for msg in value.ns_rwset {
+            let ns_rw_set = NsRwSet::try_from(msg)?;
             ns_rw_sets.push(ns_rw_set);
         }
 
@@ -40,24 +35,28 @@ impl TxRwSet {
     }
 }
 
-fn nsRwSetFromProtoMsg(msg: NsReadWriteSet) -> Result<NsRwSet> {
-    let kv_rw_set = utils::proto::unmarshal::<KvrwSet>(&msg.rwset)?;
-    let mut coll_rw_set_list = Vec::with_capacity(msg.collection_hashed_rwset.len());
+impl TryFrom<NsReadWriteSet> for NsRwSet {
+    type Error = Error;
 
-    for protoMsg in msg.collection_hashed_rwset {
-        let coll_rw_set = CollHashedRwSet{
-            CollectionName: protoMsg.collection_name,
-            HashedRwSet: utils::proto::unmarshal::<HashedRwSet>(&protoMsg.hashed_rwset)?,
-            PvtRwSetHash: protoMsg.pvt_rwset_hash
+    fn try_from(value: NsReadWriteSet) -> Result<Self> {
+        let kv_rw_set = utils::proto::unmarshal::<KvrwSet>(&value.rwset)?;
+        let mut coll_rw_set_list = Vec::with_capacity(value.collection_hashed_rwset.len());
+
+        for protoMsg in value.collection_hashed_rwset {
+            let coll_rw_set = CollHashedRwSet{
+                CollectionName: protoMsg.collection_name,
+                HashedRwSet: utils::proto::unmarshal::<HashedRwSet>(&protoMsg.hashed_rwset)?,
+                PvtRwSetHash: protoMsg.pvt_rwset_hash
+            };
+            coll_rw_set_list.push(coll_rw_set);
         };
-        coll_rw_set_list.push(coll_rw_set);
-    };
 
-    let ns_rw_set = NsRwSet{
-        NameSpace: msg.namespace,
-        KvRwSet: kv_rw_set,
-        CollHashedRwSets: coll_rw_set_list,
-    };
+        let ns_rw_set = NsRwSet{
+            NameSpace: value.namespace,
+            KvRwSet: kv_rw_set,
+            CollHashedRwSets: coll_rw_set_list,
+        };
 
-    Ok(ns_rw_set)
+        Ok(ns_rw_set)
+    }
 }
