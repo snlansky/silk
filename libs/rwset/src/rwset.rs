826b1defcf3e5c3e8929fa7a84925b89957daab4
snlansky <snlan@live.cn>
2020-04-28 19:41:03 +0800

    rename var

--- libs/rwset/src/rwset.rs
+++ libs/rwset/src/rwset.rs
@@ -4,21 +4,21 @@ use std::convert::TryFrom;
 
 // TxRwSet acts as a proxy of 'rwset.TxReadWriteSet' proto message and helps constructing Read-write set specifically for KV data model
 pub struct  TxRwSet {
-    pub NsRwSets :Vec<NsRwSet>,
+    pub ns_rw_sets:Vec<NsRwSet>,
 }
 
 // NsRwSet encapsulates 'kvrwset.KVRWSet' proto message for a specific name space (chaincode)
 pub struct NsRwSet {
-    pub NameSpace:        String,
-    pub KvRwSet          :KvrwSet ,
-    pub CollHashedRwSets :Vec<CollHashedRwSet>,
+    pub namespace:        String,
+    pub kv_rw_set:KvrwSet ,
+    pub coll_hashed_rw_sets:Vec<CollHashedRwSet>,
 }
 
 // CollHashedRwSet encapsulates 'kvrwset.HashedRWSet' proto message for a specific collection
 pub struct  CollHashedRwSet {
-    pub CollectionName: String,
-    pub HashedRwSet    :HashedRwSet,
-    pub PvtRwSetHash   :Vec<u8>,
+    pub collection_name: String,
+    pub hashed_rw_set:HashedRwSet,
+    pub pvt_rw_set_hash: Vec<u8>,
 }
 
 impl TryFrom<TxReadWriteSet> for TxRwSet {
@@ -31,7 +31,7 @@ impl TryFrom<TxReadWriteSet> for TxRwSet {
             ns_rw_sets.push(ns_rw_set);
         }
 
-        Ok(TxRwSet{ NsRwSets: ns_rw_sets })
+        Ok(TxRwSet{ ns_rw_sets })
     }
 }
 
@@ -40,21 +40,21 @@ impl TryFrom<NsReadWriteSet> for NsRwSet {
 
     fn try_from(value: NsReadWriteSet) -> Result<Self> {
         let kv_rw_set = utils::proto::unmarshal::<KvrwSet>(&value.rwset)?;
-        let mut coll_rw_set_list = Vec::with_capacity(value.collection_hashed_rwset.len());
+        let mut coll_hashed_rw_sets = Vec::with_capacity(value.collection_hashed_rwset.len());
 
         for protoMsg in value.collection_hashed_rwset {
             let coll_rw_set = CollHashedRwSet{
-                CollectionName: protoMsg.collection_name,
-                HashedRwSet: utils::proto::unmarshal::<HashedRwSet>(&protoMsg.hashed_rwset)?,
-                PvtRwSetHash: protoMsg.pvt_rwset_hash
+                collection_name: protoMsg.collection_name,
+                hashed_rw_set: utils::proto::unmarshal::<HashedRwSet>(&protoMsg.hashed_rwset)?,
+                pvt_rw_set_hash: protoMsg.pvt_rwset_hash
             };
-            coll_rw_set_list.push(coll_rw_set);
+            coll_hashed_rw_sets.push(coll_rw_set);
         };
 
         let ns_rw_set = NsRwSet{
-            NameSpace: value.namespace,
-            KvRwSet: kv_rw_set,
-            CollHashedRwSets: coll_rw_set_list,
+            namespace: value.namespace,
+            kv_rw_set,
+            coll_hashed_rw_sets,
         };
 
         Ok(ns_rw_set)
