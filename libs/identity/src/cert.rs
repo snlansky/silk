c990b212f826e3b492cf72cac48591a57ba69418
snlansky <snlan@live.cn>
2020-06-20 18:04:05 +0800

    fix test

--- libs/identity/src/cert.rs
+++ libs/identity/src/cert.rs
@@ -28,17 +28,17 @@ impl Default for Register {
     }
 }
 
-//
-// pub fn default_params() -> CertificateParams {
-//     let mut params = CertificateParams::new(vec![
-//         "crabs.crabs".to_string(), "localhost".to_string(),
-//     ]);
-//
-//     params.distinguished_name.push(DnType::OrganizationName, "Crab widgits SE");
-//     params.distinguished_name.push(DnType::CommonName, "Master CA");
-//     // params.alg = &PKCS_ED25519;
-//     params
-// }
+
+pub fn default_params() -> CertificateParams {
+    let mut params = CertificateParams::new(vec![
+        "crabs.crabs".to_string(), "localhost".to_string(),
+    ]);
+
+    params.distinguished_name.push(DnType::OrganizationName, "Crab widgits SE");
+    params.distinguished_name.push(DnType::CommonName, "Master CA");
+    // params.alg = &PKCS_ED25519;
+    params
+}
 
 
 
